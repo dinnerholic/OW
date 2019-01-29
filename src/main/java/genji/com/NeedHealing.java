6cca64a24737dba5afc675dd329c587fbb7d89db
Dinner <dinnerholic@gmail.com>
2019-01-29 16:24:39 +0800

    Java & Kotlin variable, data type

--- src/main/java/genji/com/NeedHealing.java
+++ src/main/java/genji/com/NeedHealing.java
@@ -5,5 +5,13 @@ public class NeedHealing {
 //        System.out.println("Need Healing");
         Hanzo Q = new Hanzo();
         Q.NeedHealing();
+        int age = 19;
+        Integer age2 = 19;
+        char c ='中';
+        byte b = 120 ;
+        float weight = 66.5f;
+        boolean entroll = true;
+        System.out.println(entroll);
+
     }
 }
